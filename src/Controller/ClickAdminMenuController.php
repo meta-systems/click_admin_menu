d0a97890af3f0edf43794c0bd12d105c224487f5
vitalcrazz <zojl@yandex.ru>
2018-11-01 12:01:51 +0300

    fix added links & labels

--- src/Controller/ClickAdminMenuController.php
+++ src/Controller/ClickAdminMenuController.php
@@ -25,15 +25,31 @@ class ClickAdminMenuController extends ControllerBase
         return $referer ? new RedirectResponse($referer) : $this->redirect('<front>');
     }
 
+    protected function getReferer(Request $request)
+    {
+        // Получаем урл реферера
+        $referer = $request->headers->get('referer');
+        if(!$referer) {
+            return false;
+        }
+        $url = parse_url($referer);
+        if($url === false) {
+            return false;
+        }
+        return $url['path'] . (array_key_exists('query', $url) ? "?{$url['query']}" : '');
+    }
+
     public function addPage(Request $request)
     {
-        // Получаем урл реферера.
-        $referer = $request->headers->get('referer');
+        $referer = $this->getReferer($request);
         if(!$referer) {
             return $this->redirect('<front>');
         }
 
-        $label = $request->get('title', $referer);
+        $label = $request->get('title');
+        if(empty($label)) {
+            $label = $referer;
+        }
         $entity = Example::create([
             'id' => 'auto_'.time(),
             'link' => $referer,
